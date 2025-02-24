385a54e9e97075d03bfd806a374fba89f3aad809
needfreewins <85315122+needfreewins@users.noreply.github.com>
2025-02-23 22:30:00 -0700

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -1,31 +1,31 @@
 const losers = [ 
     {
-        src: "https://cdn.discordapp.com/attachments/1339982036613599352/1342737444998479942/Branky_Unicorn.png?ex=67bab931&is=67b967b1&hm=4b44bfc05bf5dabbf86a2d34ef8e0ec8b2b7f05f72bda5ceba8d80c21a081a38&",
+        src: "https://i.ibb.co/qF7pRG5s/Branky-Unicorn.png",
         link: "https://www.reddit.com/user/Wise_Pomegranate_713/",
         name: "Branky"
     },
     { 
-        src: "https://cdn.discordapp.com/attachments/1339982036613599352/1342737446017699840/cray.png?ex=67bcb371&is=67bb61f1&hm=56a7d585418c266850032b5484f83aebc44bf259956e1bd867e1df4542825165&",
+        src: "https://i.ibb.co/sdPVttJG/cray.png",
         link: "",
         name: "Cray"
     },
     {
-        src: "https://cdn.discordapp.com/attachments/1339982036613599352/1342737447074795680/dum_lil_defeatslut.png?ex=67bcb372&is=67bb61f2&hm=095e4b34c9045fa5251258c5dfc548f6c9455ac0bdc1b0fbc497b2bea03a4c58&",
+        src: "https://i.ibb.co/1JzXccvb/dum-lil-defeatslut.png",
         link: "https://www.reddit.com/user/dum_lil_defeatslut/",
         name: "dum_lil_defeatslut"
     },
     {
-        src: "https://cdn.discordapp.com/attachments/1339982036613599352/1342737447972245664/Teal-octopus.png?ex=67bcb372&is=67bb61f2&hm=ffa98f7b14a0ee633ea76d8227c4b7b654272a4b266f580e557744c84a6da01f&",
+        src: "https://i.ibb.co/XrMCXDr0/Teal-octopus.png",
         link: "https://www.reddit.com/user/Teal-octopus/",
         name: "Teal Octopus"
     },
     {
-        src: "https://cdn.discordapp.com/attachments/1339982036613599352/1342737444122136617/boundinwonderland.png?ex=67bcb371&is=67bb61f1&hm=1dde4151ee17210df711341de9f3b044a18a9dc50498738e7b35a599058a0303&",
+        src: "https://i.ibb.co/QqM7pFh/boundinwonderland.png",
         link: "https://www.reddit.com/user/boundinwonderland/",
         name: "boundinwonderland"
     },
     {
-        src: "https://cdn.discordapp.com/attachments/1339982036613599352/1343443472862150790/rp___babe.png?ex=67bd4abb&is=67bbf93b&hm=4d6c210dfde1a4632aea4293602ebe8e4741860b141f368ea17e025a0396cce9&",
+        src: "https://i.ibb.co/M5Gd2ZZr/rp-babe.png",
         link: "https://www.reddit.com/user/rp___babe/",
         name: "rp___babe"
     }
@@ -33,7 +33,7 @@ const losers = [
 
 const winners = [
     {
-        src: "https://cdn.discordapp.com/attachments/1339982036613599352/1342737442972897321/blazey.png?ex=67bcb371&is=67bb61f1&hm=6bf01fe01292ee39f425a2eea73d7f3f56e247fee980ade9fcb973e1d56c2a69&",
+        src: "https://i.ibb.co/zVPLdYHY/blazey.png",
         link: "https://www.reddit.com/user/MysticalBlaze22/",
         name: "Blazey"
     },
